054b038ef086838b1e73b7630c3cadb5e6d732a4
Mado007 <mahmodeid2019@gmail.com>
2025-03-19 23:13:06 +0200

    add serve html for vercel host

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'src', 'static')));
 app.use('/assets', express.static(path.join(__dirname, 'src', 'assets')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+// add route to serve the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/static/index.html'));
+});
 
 // Endpoint to get environment variables
 app.get('/env', (req, res) => {
